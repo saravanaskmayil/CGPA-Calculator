7fc67cf970245f28dcf8e4d96c712a4ae22489de
saravanaskmayil <85149057+saravanaskmayil@users.noreply.github.com>
2021-07-31 14:18:43 +0530

    Add files via upload

---
+++ cgpa.js
@@ -0,0 +1,48 @@
+
+//  function cgpa (){
+//   let first_cgpa = document.querySelector("#first-cgpa").value;
+// //   console.log(document.querySelector('#first-cgpa').value);
+
+//   let second_cgpa = document.querySelector("#second-cgpa").value;
+
+//   let third_cgpa = document.querySelector("#third-cgpa").value;
+
+//   let fourth_cgpa = document.querySelector("#fourth-cgpa").value;
+
+//   let fifth_cgpa = document.querySelector("#fifth-cgpa").value;
+
+//   let sixth_cgpa = document.querySelector("#sixth-cgpa").value;
+
+//   let seventh_cgpa = document.querySelector("#seventh-cgpa").value;
+
+//   let eight_cgpa = document.querySelector("#eighth-cgpa").value;
+
+
+//   let total =
+//     parseFloat(first_cgpa) +
+//     parseFloat(second_cgpa) +
+//     parseFloat(third_cgpa) +
+//     parseFloat(fourth_cgpa) +
+//     parseFloat(fifth_cgpa) +
+//     parseFloat(sixth_cgpa) +
+//     parseFloat(seventh_cgpa) +
+//     parseFloat(eight_cgpa); //upto 8th sem;
+//     let total_cgpa = total/8;
+//       total_cgpa = total_cgpa.toPrecision(2);
+//   document.querySelector(".cgpa-output h1").innerHTML =
+//     `Total CGPA =  ${total_cgpa}`;
+// };
+
+//     function reset (){
+//   document.querySelector("#first-cgpa").value = " ";
+//   document.querySelector("#second-cgpa").value = " ";
+//   document.querySelector("#third-cgpa").value = "";
+//   document.querySelector("#fourth-cgpa").value = "  ";
+//   document.querySelector("#fifth-cgpa").value = "  ";
+//   document.querySelector("#sixth-cgpa").value = "  ";
+//   document.querySelector("#seventh-cgpa").value = "  ";
+//   document.querySelector("#eighth-cgpa").value = "  ";
+// };
+
+
+
